85611b1f5a72ee74d4c51129ad00dfce49f7d964
Geza Csosz <geza.csosz@gmail.com>
2025-01-10 14:09:33 +0000

    add css

--- art/views.py
+++ art/views.py
@@ -1,11 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
 
-
 # Create your views here.
+
+
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
-    template_name = "post_list.html"
-    
+    template_name = "art/index.html"
+    paginate_by = 6
 
+
+def post_detail(request, slug):
+    
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+    return render(request, "art/post_detail.html", {"post": post},)
