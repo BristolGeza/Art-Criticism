ad13aed00f4d05fcc2029cefefc1bc048aba29c9
Geza Csosz <geza.csosz@gmail.com>
2025-01-10 16:10:38 +0000

    add pic and fixed the bugs

--- codestar/urls.py
+++ codestar/urls.py
@@ -2,9 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 
-
 urlpatterns = [
-     path("about/", include("about.urls"), name="about-urls"),
+    path("about/", include("about.urls"), name="about-urls"),
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path("", include("art.urls"), name="art-urls"),
