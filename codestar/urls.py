d7f73db55d466be8235bc8c8dd56b08639d198a3
Geza Csosz <geza.csosz@gmail.com>
2025-01-13 11:56:51 +0000

    add tx

--- codestar/urls.py
+++ codestar/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("about/", include("about.urls"), name="about-urls"),
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
+    path("accounts/", include("allauth.urls")),
     path("", include("art.urls"), name="art-urls"),
 ]
