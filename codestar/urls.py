2aa0488209e7de9438504b04558b5af74581c247
Geza Csosz <geza.csosz@gmail.com>
2025-01-15 11:28:23 +0000

    add media

--- codestar/urls.py
+++ codestar/urls.py
@@ -11,4 +11,4 @@ urlpatterns = [
     path("", include("art.urls"), name="art-urls"),
 ]
 
-urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
