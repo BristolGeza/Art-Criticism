0808a446a033435ee8889cdcce3d0451decfef3a
Geza Csosz <geza.csosz@gmail.com>
2025-01-21 10:58:14 +0000

    change views.py

--- art/migrations/0009_collaboraterequest.py
+++ art/migrations/0009_collaboraterequest.py
@@ -19,7 +19,7 @@ class Migration(migrations.Migration):
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('title', models.CharField(max_length=200)),
                 ('slug', models.SlugField(unique=True)),
-                ('banner', models.ImageField(upload_to='banners/')),
+                ('banner', models.ImageField(upload_to='media/')),
                 ('content', models.TextField()),
                 ('author', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
