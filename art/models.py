0c78fe92a9b6fd4ec9aa93809a516df8dc57fb3f
Geza Csosz <geza.csosz@gmail.com>
2025-04-09 14:55:40 +0100

    Update models.py

--- art/models.py
+++ art/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 from django.utils import timezone
 from django.utils.text import slugify
+from django.core.exceptions import ValidationError
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
@@ -48,6 +49,8 @@ class Comment(models.Model):
 
 
 # Geza: Collaborate Request Model
+from django.core.exceptions import ValidationError
+
 class CollaborateRequest(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True, blank=True)
@@ -63,10 +66,12 @@ class CollaborateRequest(models.Model):
         ordering = ["-created_on"]
 
     def save(self, *args, **kwargs):
-        if not self.slug:  # Generate slug only if not set
+        if not self.pk and CollaborateRequest.objects.filter(title=self.title).exists():
+            raise ValidationError("You have to change the Title, because it exists")
+
+        if not self.slug:
             self.slug = slugify(self.title)
         super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.title} | written by {self.author}"
-
