3409d07d03ef7cd47d39f4e2b63ac798ed7da100
Geza Csosz <geza.csosz@gmail.com>
2025-06-02 15:29:58 +0100

    Update models.py

--- art/models.py
+++ art/models.py
@@ -15,7 +15,7 @@ class Post(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="art_posts"
     )
-   # banner = models.ImageField(default='default.jpg', blank=True)
+    banner = models.ImageField(default='default.jpg', blank=True)
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
